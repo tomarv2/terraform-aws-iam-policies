56f5825cb1d8f0429a9f7bd8f6cf9f3e090f5221
tomarv2 <varuntomar2019@gmail.com>
2021-02-18 20:30:32 -0800

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version            = ">= 0.14"
   required_providers {
     aws = {
-      version                     = "~> 2.61"
+      version                 = "~> 3.0"
     }
   }
 }
@@ -10,8 +10,4 @@ terraform {
 provider "aws" {
   region                      = var.aws_region
   profile                     = var.profile_to_use
-}
-
-module "global" {
-  source                      = "git::git@github.com:tomarv2/terraform-global.git//aws?ref=0.0.1"
 }
